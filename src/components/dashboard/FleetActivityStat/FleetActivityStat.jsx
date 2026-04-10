7fa9798edffa70f99a1bab2fa2d4a99a0abcf0be
travismichaelfain <travismichaelfain@gmail.com>
2026-04-10 09:25:20 -0700

    prop name error for decom ship stat

--- src/components/dashboard/FleetActivityStat/FleetActivityStat.jsx
+++ src/components/dashboard/FleetActivityStat/FleetActivityStat.jsx
@@ -4,13 +4,13 @@ import styles from "./FleetActivityStat.module.css";
 const FleetActivityStat = ({
   voyages,
   spacecraftsBuilt,
-  decommissionedShips,
+  decommissionedSpacecrafts,
 }) => {
   return (
     <div className={styles.fleetActivityStat}>
       <p>Number of Voyages: {voyages}</p>
       <p>Spacecrafts Built: {spacecraftsBuilt}</p>
-      <p>Decommissioned Spacecrafts: {decommissionedShips}</p>
+      <p>Decommissioned Spacecrafts: {decommissionedSpacecrafts}</p>
     </div>
   );
 };
