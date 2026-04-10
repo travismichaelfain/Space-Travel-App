7472d632c1e83d8081532c190dfeca9d1f24cf61
travismichaelfain <travismichaelfain@gmail.com>
2026-04-10 09:49:59 -0700

    prop name mismatch fleet usage

--- src/components/dashboard/FleetUsageStat/FleetUsageStat.jsx
+++ src/components/dashboard/FleetUsageStat/FleetUsageStat.jsx
@@ -1,9 +1,13 @@
 import styles from "./FleetUsageStat.module.css";
 
-const FleetUsageStat = ({ utilizationRate, activeShips, totalSpacecrafts }) => {
+const FleetUsageStat = ({
+  utilizationRate,
+  activeSpacecrafts,
+  totalSpacecrafts,
+}) => {
   return (
     <div className={styles.fleetUsageStat}>
-      <p>Active Spacecrafts: {activeShips}</p>
+      <p>Active Spacecrafts: {activeSpacecrafts}</p>
       <p>Operational Spacecrafts: {totalSpacecrafts}</p>
       <p>Utilization Rate: {utilizationRate}%</p>
     </div>
